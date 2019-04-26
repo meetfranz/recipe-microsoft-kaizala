d4d293012bbb2ea410dedc402e4aa7462ccb292e
Stefan Malzner <stefan@adlk.io>
2019-04-26 15:08:46 +0200

    initial commit

---
+++ webview.js
@@ -0,0 +1,9 @@
+module.exports = (Franz) => {
+  const getMessages = function getMessages() {
+    const count = document.querySelectorAll('.unseen-msg-count').length;
+
+    Franz.setBadge(count);
+  };
+
+  Franz.loop(getMessages);
+};
